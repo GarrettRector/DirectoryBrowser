e75923f7745f5bce0d9a0f445230d99990a3feb8
garrettrector <garrett@grector.dev>
2021-12-08 13:07:44 -0500

    added showdir

--- src/browser.js
+++ src/browser.js
@@ -10,39 +10,60 @@ const readline = require('readline').createInterface({
 
 readline.on("line", scanDirectory);
 let currentDirectory = __dirname;
+console.log("Current Directory:")
+promptUser(true).then();
 
-promptUser();
-
-function promptUser() {
+async function promptUser(doread) {
+    if (doread) {
+        await read()
+    }
     readline.setPrompt("\nPath? > ");
     readline.prompt();
 }
 
 async function scanDirectory(userInput) {
     let path = userInput.trim().toLowerCase();
+    if (path === "showdir") {
+        console.log("\nCurrent Directory:")
+        await read()
+        return promptUser()
+    }
+
     const goUp = path === "up";
     currentDirectory = resolve(currentDirectory, goUp ? ".." : path);
 
+
     try {
         if (fs.statSync(paths.join(currentDirectory)).isFile()) {
-            fs.readFile(currentDirectory, (err, data) => {
+            fs.readFile(currentDirectory, async (err, data) => {
                 if (err) throw err
-
-                console.log(data.toString())
-                promptUser();
+                data = data.toString();
+                if (data === "") {
+                    data = "File is empty\n";
+                }
+                console.log(data);
+                currentDirectory = paths.join(currentDirectory, "..")
+                await promptUser(false);
             })
         } else {
-            const files = await readdir(currentDirectory);
-            files.forEach(function (file) {
-                let stats = fs.statSync(paths.join(currentDirectory, file));
-                let isfile;
-                if (stats.isFile()) { isfile = "File"; } else { isfile = "Directory"; }
-                console.log(`|\n| - ${file} (${isfile})`);
-            });
-            promptUser();
+            await promptUser(true);
         }
     } catch (error) {
         console.log(`Directory "${userInput}" does not exist!`);
         return readline.close();
     }
+}
+
+async function read() {
+    const files = await readdir(currentDirectory);
+    files.forEach(function (file) {
+        let stats = fs.statSync(paths.join(currentDirectory, file));
+        let isfile;
+        if (stats.isFile()) {
+            isfile = "File";
+        } else {
+            isfile = "Directory";
+        }
+        console.log(`|\n| - ${file} (${isfile})`);
+    })
 }
