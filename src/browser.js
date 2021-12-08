14b73033a1a6538b2f7f99fc436e87ccf21c8204
garrettrector <garrett@grector.dev>
2021-12-07 22:21:42 -0500

    Lists dir or file

--- src/browser.js
+++ src/browser.js
@@ -1,50 +1,43 @@
-const local_path = require("path")
-const fs = require("fs")
-const browser = require("./browser");
+const { resolve } = require("path");
+const paths = require("path")
+const { readdir } = require("fs/promises");
+const fs = require("fs");
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
 
-function get_dirs(path) {
-    let dir_path;
-    if (path.toLowerCase() === "up") {
-        let path_ = local_path.join(__dirname).split("/");
-        path_.pop();
-        dir_path = path_.join("/");
-    } else {
-        dir_path = local_path.join(__dirname, path);
-    }
-    fs.readdir(dir_path, function (err, files) {
-        if (err) {
-            return console.log("Unable to scan directory!")
-        }
+readline.on("line", scanDirectory);
+let currentDirectory = __dirname;
 
+promptUser();
+
+function promptUser() {
+    readline.setPrompt("\nPath? > ");
+    readline.prompt();
+}
+
+async function scanDirectory(userInput) {
+    const path = userInput.trim().toLowerCase();
+    const goUp = path === "up";
+    currentDirectory = resolve(currentDirectory, goUp ? ".." : path);
+
+    try {
+        const files = await readdir(currentDirectory);
         files.forEach(function (file) {
-            console.log(
-                `|\n| - ${file}`
-            )
+            let stats = fs.statSync(paths.join(currentDirectory, file));
+            let isfile;
+            if (stats.isFile()) {
+                isfile = "File";
+            } else {
+                isfile = "Directory";
+            }
+            console.log(`|\n| - ${file} (${isfile})`);
         });
-    });
+        promptUser();
+    } catch (error) {
+        console.log(`Directory "${userInput}" does not exist!`);
+        return readline.close();
+    }
 }
-
-exports.question = function get_input() {
-    let response;
-
-    readline.setPrompt("Path? > ")
-    readline.prompt()
-
-    return new Promise((resolve) => {
-        readline.on('line', (userInput) => {
-            response = userInput;
-            readline.close();
-        });
-
-        readline.on('close', () => {
-            resolve(response);
-        });
-    })
-}
-
-browser.question().then(response => get_dirs(response))
